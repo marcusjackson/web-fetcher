2d56da45f6e7c38c990cd6eded527777a940a535
Marcus Jackson <marcus.fm.jackson@gmail.com>
2023-02-12 22:08:00 +0900

    Plug in argument flag parser into main for checking for metadata flag.

--- fetch/main.rb
+++ fetch/main.rb
@@ -3,6 +3,7 @@
 require_relative 'lib/web_page'
 require_relative 'lib/web_page_writer'
 require_relative 'lib/web_page_metadata'
+require_relative 'lib/argument_flag_parser'
 
 def fetch(url)
   web_page = WebPage.new(url)
@@ -25,15 +26,16 @@ def write_metadata(web_page)
   ).metadata
 end
 
-def main(urls)
+def main(has_metadata_flag, urls)
   urls.each do |url|
     web_page = fetch(url)
 
     if web_page.html
       write_to_file(web_page)
-      write_metadata(web_page)
+      write_metadata(web_page) if has_metadata_flag
     end
   end
 end
 
-main(ARGV)
+has_metadata_flag, urls = ArgumentFlagParser.check_for_metadata_flag(ARGV)
+main(has_metadata_flag, urls)

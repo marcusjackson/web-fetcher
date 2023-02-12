299bc4643884bc233fb6996402272c250f272d27
Marcus Jackson <marcus.fm.jackson@gmail.com>
2023-02-12 20:31:59 +0900

    Add web page writer class and implementation up to writing a file.

---
+++ fetch/lib/web_page_writer.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# Represents the web pages to be fetched
+class WebPageWriter
+  TEMP_DIR = 'temp'
+
+  def initialize(host, html, write_metadata: true)
+    @host = host
+    @html = html
+    @write_metadata = write_metadata
+  end
+
+  def write_file
+    result = File.write(File.join(TEMP_DIR, "#{@host}.html"), @html.to_s)
+    write_metadata if @write_metadata
+    result
+  end
+
+  def write_metadata
+    puts "site: #{@host}"
+  end
+end

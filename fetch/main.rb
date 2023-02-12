4256639dbb227ec717adddc911eb4c0767b876b0
Marcus Jackson <marcus.fm.jackson@gmail.com>
2023-02-12 21:21:47 +0900

    Plug in web page, web page writer, and web page metadata classes into main.

--- fetch/main.rb
+++ fetch/main.rb
@@ -1,5 +1,39 @@
 # frozen_string_literal: true
 
-ARGV.each do |url|
-  puts url
+require_relative 'lib/web_page'
+require_relative 'lib/web_page_writer'
+require_relative 'lib/web_page_metadata'
+
+def fetch(url)
+  web_page = WebPage.new(url)
+  web_page.fetch_html
+  web_page
 end
+
+def write_to_file(web_page)
+  WebPageWriter.new(
+    web_page.host,
+    web_page.html
+  ).write_file
+end
+
+def write_metadata(web_page)
+  puts WebPageMetadata.new(
+    web_page.host,
+    web_page.html,
+    Time.now.utc
+  ).metadata
+end
+
+def main(urls)
+  urls.each do |url|
+    web_page = fetch(url)
+
+    if web_page.html
+      write_to_file(web_page)
+      write_metadata(web_page)
+    end
+  end
+end
+
+main(ARGV)

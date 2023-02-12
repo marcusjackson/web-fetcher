fccfd0f045645eb4f2dfdbea6d0cc96884e9635a
Marcus Jackson <marcus.fm.jackson@gmail.com>
2023-02-12 21:19:05 +0900

    Move temporary metadata writing out of web page writer class.

--- fetch/lib/web_page_writer.rb
+++ fetch/lib/web_page_writer.rb
@@ -4,19 +4,12 @@
 class WebPageWriter
   TEMP_DIR = 'temp'
 
-  def initialize(host, html, write_metadata: true)
+  def initialize(host, html)
     @host = host
     @html = html
-    @write_metadata = write_metadata
   end
 
   def write_file
-    result = File.write(File.join(TEMP_DIR, "#{@host}.html"), @html.to_s)
-    write_metadata if @write_metadata
-    result
-  end
-
-  def write_metadata
-    puts "site: #{@host}"
+    File.write(File.join(TEMP_DIR, "#{@host}.html"), @html.to_s)
   end
 end
